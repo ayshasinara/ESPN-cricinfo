faf06dec05de180c017d1dfe02766168807c1276
Swapnil Parchake <swapnilparchakewrd2@gmail.com>
2022-08-25 21:21:29 +0530

    schedule Card

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 function App() {
   return (
     <div className="App">
-    <ChakraProvider>
-    <SchedualCard/>
-    </ChakraProvider>
+
     </div>
   );
 }
