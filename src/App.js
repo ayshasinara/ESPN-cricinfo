b60d8f8bb4be49baa1766a6e325d33d0dfd4e8ff
vedantpawar18 <vedantpawar18@gmail.com>
2022-08-28 13:09:32 +0530

    ad

--- src/App.js
+++ src/App.js
@@ -6,14 +6,13 @@ import Footer from './Pages/Footer';
 import News from './Pages/News';
 import { SchedualFilter } from './Components/Schedual/SchedualFilter';
 import MainRoutes from './Pages/MainRoutes';
+import { Carousel1 } from './Components/Schedual/Carousel1';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <MainRoutes/>
-      <Footer/>
       </div>
   );
 }
--- src/Components/news/NewsContainer.jsx
+++ src/Components/news/NewsContainer.jsx
@@ -10,7 +10,7 @@ function NewsContainer() {
   useEffect(() => {
     dispatch(getNewsData());
   }, [])
-
+console.log(data)
   return (
     <MainWrapper>
       <HeadingWrapper>
